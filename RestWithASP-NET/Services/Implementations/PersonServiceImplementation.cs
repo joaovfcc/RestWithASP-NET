4d914a30ce4020cd64c3937b1d5f805487b2e53a
Joao Vitor Farias <vitorfchaves1@gmail.com>
2025-09-03 16:45:25 -0300

    Implementacao da interface IPersonService

---
+++ RestWithASP-NET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,38 @@
+﻿using RestWithASP_NET.Model;
+
+namespace RestWithASP_NET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Leandro",
+                LastName = "Costa",
+                Address = "Uberlândia - Minas Gerais - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
