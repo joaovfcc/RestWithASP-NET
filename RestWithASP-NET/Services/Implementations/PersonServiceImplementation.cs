e20428000a16684aff2136e4e870453d543c9b3e
Joao Vitor Farias <vitorfchaves1@gmail.com>
2025-09-04 19:29:46 -0300

    Correcao da classe PersonServiceImplementation

--- RestWithASP-NET/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithASP-NET/Services/Implementations/PersonServiceImplementation.cs
@@ -9,37 +9,78 @@ namespace RestWithASP_NET.Services.Implementations
         private MySQLContext _context;
 
         public PersonServiceImplementation(MySQLContext context)
-        { 
+        {
             _context = context;
-        
-        }
 
-        public Person Create(Person person)
-        {
-            return person;
         }
-
-        public void Delete(long id)
-        {
-        }
-
         public List<Person> FindAll()
         {
 
             return _context.Persons.ToList();
         }
-        
 
         public Person FindById(long id)
         {
-            // Retorna uma instância de Person usando o construtor correto
-            return new Person(id, "Leandro", "Costa", "Uberlândia - Minas Gerais - Brasil", "Male");
+            return _context.Persons.FirstOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return person;
         }
 
         public Person Update(Person person)
         {
-            return person;
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return person;
+            }
+
+            return new Person();
         }
-   
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
     }
 }
